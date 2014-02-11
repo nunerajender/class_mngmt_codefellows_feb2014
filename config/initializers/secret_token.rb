530b3a892dc9a166b65606baae50fee5ef56e4c3
Renée Hendricksen <renee@nird.us>
2014-02-10 19:49:44 -0800

    rails new class_mngmt bare rails app to track all of our changes going forward

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassMngt::Application.config.secret_key_base = 'b44d2707c7d99fce68ce2cf896e32e36bfb3a3d54ce24fe28b3194fbd616aa726cf2c967a5dc6201622a26bb6f6b5025a27fe1f70234924c806748e872107fef'
