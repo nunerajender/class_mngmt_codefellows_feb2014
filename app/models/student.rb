ed6cc2e2b324e04b607d77ba7d452ccb166dc203
Renée Hendricksen <renee@nird.us>
2014-02-12 21:03:50 -0800

    fixed the student additional functionality which we saw was broken in the rails console added an uncountable inflection for the word shorts

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,12 +2,12 @@ class Student < ActiveRecord::Base
 
   def combined_info
     "name: #{full_name} \n"+
-    "age: #{age}"
-    "bio: #{bio} \n"+
+    "age: #{age} \n"+
+    "bio: #{bio}"
   end
 
   def age_in_60_years
-    age + 60.years
+    (age + 60).years
   end
 
 end
