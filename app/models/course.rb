032caf02ea7822ac81bd916b4cc28e75e28d35ed
Renée Hendricksen <renee@nird.us>
2014-02-19 15:44:34 -0800

    Monday's class work on a many to many relationship - we changed the one-to-many for student - courses into a many-to-many students - courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
 class Course < ActiveRecord::Base
 
-  belongs_to :student
+  #belongs_to :student
+  has_many :registrations, class_name: Registration, foriegn_key: :course_id
+  has_many :students, through: :registrations
 
 end
