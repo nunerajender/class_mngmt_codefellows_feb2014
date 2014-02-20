25266ca4197dff31fc7f85c800898c9aab054c1a
Renée Hendricksen <renee@nird.us>
2014-02-19 17:05:10 -0800

    fix the spelling error on course relationship

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
 
   #belongs_to :student
-  has_many :registrations, class_name: Registration, foriegn_key: :course_id
+  has_many :registrations, class_name: Registration, foreign_key: :course_id
   has_many :students, through: :registrations
 
 end
