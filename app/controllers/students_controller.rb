6c3b9569ae7c6051607b0892d443d966764e6e1a
Renée Hendricksen <renee@nird.us>
2014-02-26 21:53:03 -0800

    allowing the selection and editing of courses for a student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -74,6 +74,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:full_name, :age, :bio, :title, :company, :favorite_ice_cream, :gender)
+      params.require(:student).permit({course_ids: []},  :full_name, :age, :bio, :title, :company, :favorite_ice_cream, :gender, :email)
     end
 end
